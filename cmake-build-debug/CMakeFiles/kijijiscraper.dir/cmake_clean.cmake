c5b7d61975b7efaf854615cd71b256341ba8f824
Nathaniel Roberts <nathanieljroberts11@gmail.com>
2020-05-06 12:02:22 -0700

    cmake debug added

---
+++ cmake-build-debug/CMakeFiles/kijijiscraper.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kijijiscraper.dir/spider.c.o"
+  "kijijiscraper.exe"
+  "kijijiscraper.pdb"
+  "libkijijiscraper.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kijijiscraper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
