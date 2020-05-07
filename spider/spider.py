2a48fa5dde443466559986a932df82101be9142a
Nathaniel Roberts <nathanieljroberts11@gmail.com>
2020-05-06 20:43:33 -0700

    spider grabs url and reads from file

---
+++ spider/spider.py
@@ -0,0 +1,16 @@
+import random
+import time
+import urllib.request
+from urllib.request import HTTPError
+
+for i in range(14570000, 20000000):
+	try:
+		url = "https://www.kijijiautos.ca/vip/" + str(i) + "/"
+		x = urllib.request.urlopen(url)
+		f = open("dump/" + str(i) + ".txt", "w")
+		f.write(str(x.read()))
+		print("url read")
+	except HTTPError:
+		print("invalid url")
+	time.sleep(random.random()+1)
+	print("reading url " + str(i+1))
