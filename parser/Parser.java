475916790f2745df63b264aee3db133e570fd7fe
Nathaniel Roberts <nathanieljroberts11@gmail.com>
2020-05-06 22:45:49 -0700

    removed dumpfolder.txt

--- parser/Parser.java
+++ parser/Parser.java
@@ -1,5 +1,8 @@
+import java.io.File;
+import java.nio.file.Files;
+
 public class Parser{
 	public static void main(String[] args){
-		
+		String dumpFolder = Files.readString("dumpfolder.txt");
 	}
 }
